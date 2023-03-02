2e3010a46662da3d88d35248f74b3ae354ebe05c
IsaacMvmv <67015742+IsaacMvmv@users.noreply.github.com>
2023-03-02 16:00:54 +0100

    Attempt to fix stuff #2

--- install.sh
+++ install.sh
@@ -118,7 +118,7 @@ SigLevel = Never
 Server = https://jlk.fjfi.cvut.cz/arch/repo' > /tmp/pacman.conf
 
 
-sudo pacman -C /tmp/pacman.conf -Sy
+sudo pacman --config /tmp/pacman.conf -Sy
 sudo pacman -S --needed arch-install-scripts parted wget archlinux-keyring blackarch-keyring chaotic-keyring
 sudo pacman -Sy
 
